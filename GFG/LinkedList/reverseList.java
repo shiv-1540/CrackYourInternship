14fa849efe8dc87b9c23239d0b32763008e27613
Shiv Ghyar <136742691+shiv-1540@users.noreply.github.com>
2024-10-26 23:15:02 +0530

    Update reverseList.java

--- GFG/LinkedList/reverseList.java
+++ GFG/LinkedList/reverseList.java
@@ -11,3 +11,14 @@
         }
         return prev;
     }
+
+Node reverseList(Node head) {
+        // code here
+        if(head==null || head.next==null){
+            return head;
+        }
+        Node newhead=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return newhead;
+    }
